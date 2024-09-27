cefacebe3706d7ccaca10c64d3da9c159e24ee36
Daniil Vinogradov <xitrix@bk.ru>
2024-09-27 23:37:05 +0300

    WIP: Act 3 - Nekit Basement

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -6,6 +6,8 @@ public class CameraScript : MonoBehaviour
     public static CameraScript Shared { get; private set; }
 
     public GameObject followedObject;
+    public GameObject overrideFollowedObject;
+    public bool followOverridenObject;
     public float cameraSpeed = 1;
 
     private Transform Transform { get; set; }
@@ -30,15 +32,28 @@ public class CameraScript : MonoBehaviour
 
     private void UpdateCameraPosition()
     {
-        if (!followedObject) return;
+        var objectToFollow = followOverridenObject ? overrideFollowedObject : followedObject;
+        if (!objectToFollow) return;
         
         var position = Transform.position;
 
-        var target = Vector3.Lerp(transform.position, followedObject.transform.position, Time.deltaTime * cameraSpeed);
+        var target = Vector3.Lerp(transform.position, objectToFollow.transform.position, Time.deltaTime * cameraSpeed);
         target.z = position.z;
         
         Transform.position = target;
 
         // Transform.position = position;
+    }
+
+    public static void StartFollowingObject(GameObject obj)
+    {
+        Shared.overrideFollowedObject = obj;
+        Shared.followOverridenObject = true;
+    }
+    
+    public static void StopFollowingObject()
+    {
+        Shared.followOverridenObject = false;
+        Shared.overrideFollowedObject = null;
     }
 }
