57d1b2f2e585d3ea7520c987c2f1e85636af6965
XITRIX <xitrix@bk.ru>
2026-08-07 00:51:13 +0200

    Act6: WIP

---
+++ Assets/Assets/Prefabs/Maps/Act6/Act6MapScript.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Act6MapScript : MonoBehaviour
+{
+    public List<GameObject> investigateObjects = new(); 
+    public List<GameObject> hideObjects = new();
+
+    public GameObject roomLight;
+    public List<GameObject> redLights = new();
+
+    public GameObject wardrobeLDore;
+    public GameObject wardrobeRDore;
+    public BoxCollider wardrobeCollider;
+
+    private static Act6MapScript _singleton;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        _singleton = this;
+    }
+
+    public static void DisableInvestigateObjects()
+    {
+        foreach (var investigateObject in _singleton.investigateObjects)
+        {
+            investigateObject.SetActive(false);
+        }
+    }
+
+    public static void SetEnableRoomLight(bool enable)
+    {
+        _singleton.roomLight.SetActive(enable);
+        foreach (var redLight in _singleton.redLights)
+        {
+            redLight.SetActive(!enable);
+        }
+    }
+
+    public static void SetWardrobeOpen(bool open)
+    {
+        _singleton.wardrobeLDore.transform.localRotation = Quaternion.Euler(0f, 0f, open ? 100f : 0f);
+        _singleton.wardrobeRDore.transform.localRotation = Quaternion.Euler(0f, 0f, open ? -100f : 0f);
+    }
+    
+    public static void SetWardrobeColliderEnabled(bool enable)
+    {
+        _singleton.wardrobeCollider.gameObject.SetActive(enable);
+    }
+}
